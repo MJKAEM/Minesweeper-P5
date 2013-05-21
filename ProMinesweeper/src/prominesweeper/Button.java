08b33aa15536ca00216e2e9a123fa4e0a371e94b
MJKAEM <Martinokuan@gmail.com>
2013-05-21 10:37:17 -0700

    Adding more functions.  Clicking isn't fully implemented.

--- ProMinesweeper/src/prominesweeper/Button.java
+++ ProMinesweeper/src/prominesweeper/Button.java
@@ -3,31 +3,79 @@ package prominesweeper;
 import java.awt.Color;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class Button
 {
 	private PApplet p;
-	
-	private boolean isBomb, isClicked;
+
+	private boolean bomb, clicked;
 	private int posX, posY;
-	private Color c;
-	
+	private int other;
+
 	public Button(PApplet America, int x, int y)
 	{
 		p = America;
-		isBomb = false;
-		posX = x * 20;
-		posY = y * 20;
-		c = Color.GRAY;
+		bomb = false;
+		clicked = false;
+		posX = x * ProMinesweeper.BUTTONSIZE;
+		posY = y * ProMinesweeper.BUTTONSIZE;
 	}
-	
-	public void Show()
+
+	public void Show(PFont f)
 	{
-		p.fill(c.getRed(), c.getGreen(), c.getBlue());
-		p.rect(posX, posY, 20, 20);
+		p.textFont(f);
+		if(clicked)
+		{
+			p.fill(255);
+		}
+		else
+		{
+			p.fill(128);
+		}
+		p.rect(posX, posY, ProMinesweeper.BUTTONSIZE, ProMinesweeper.BUTTONSIZE);
+
+		switch(other)
+		{
+		case 1:
+			p.fill(0, 0, 255);
+			p.text(1, posX, posY);
+			break;
+		case 2:
+			p.fill(0, 255, 0);
+			p.text(2, posX, posY);
+			break;
+		case 3:
+			p.fill(255, 0, 0);
+			p.text(3, posX, posY);
+			break;
+		case 4:
+			p.fill(255, 0, 255);
+			p.text(4, posX, posY);
+			break;
+		case 5:
+			p.fill(0);
+			p.text(5, posX, posY);
+			break;
+		case 6:
+			p.fill(0);
+			p.text(6, posX, posY);
+			break;
+		case 7:
+			p.fill(0);
+			p.text(7, posX, posY);
+			break;
+		case 8:
+			p.fill(0);
+			p.text(8, posX, posY);
+			break;
+		}
 	}
-	
-	public boolean GetBomb() { return isBomb; }
-	public void SetBomb() { isBomb = true; }
-	public void SetColor(Color colour) { c = colour; }
+
+	public boolean GetBomb() { return bomb; }
+	public boolean GetClicked() { return clicked; }
+	public int GetSurrounding() { return other; }
+	public void SetBomb() { bomb = true; }
+	public void SetClicked() { clicked = true; }
+	public void SetSurrounding(int surround) { other = surround; }
 }
--- ProMinesweeper/src/prominesweeper/ProMinesweeper.java
+++ ProMinesweeper/src/prominesweeper/ProMinesweeper.java
@@ -1,32 +1,38 @@
 package prominesweeper;
 
 import processing.core.PApplet;
+import processing.core.PFont;
+
 import java.util.*;
 
 public class ProMinesweeper extends PApplet
 {
-	public final static int ROWS = 10, COLS = 10;
+	public final static int ROWS = 10, COLS = 10, BUTTONSIZE = 20;
 	
+	private PFont font1 = loadFont("ArialMT-12.vlw");
 	private Button[][] button;
-	
+
 	public void setup()
 	{
-		button = new Button[ROWS][COLS];
+		textFont(font1);
+		text("Forced loading of font.", -255, -255);
 		
+		button = new Button[ROWS][COLS];
+
 		for(int row = 0; row < ROWS; row++)
 		{
 			for(int col = 0; col < COLS; col++)
 			{
 				button[row][col] = new Button(this, row, col);
-				
+
 				if(Math.random() < .1)
 				{
 					button[row][col].SetBomb();
 				}
 			}
 		}
-		
-		size((20 * ROWS) + 1, (20 * COLS) + 1);
+
+		size((BUTTONSIZE * ROWS) + 1, (BUTTONSIZE * COLS) + 1);
 	}
 
 	public void draw()
@@ -35,8 +41,47 @@ public class ProMinesweeper extends PApplet
 		{
 			for(int col = 0; col < COLS; col++)
 			{
-				button[row][col].Show();
+				button[row][col].Show(font1);
 			}
 		}
+	}
+	public void mouseReleased()
+	{
+		for(int row = 0; row < ROWS; row++)
+		{
+			for(int col = 0; col < COLS; col++)
+			{
+				if(mouseX >= row * BUTTONSIZE && mouseX <= (row * BUTTONSIZE) + BUTTONSIZE)
+				{
+					if(mouseY >= col * BUTTONSIZE && mouseY <= (col * BUTTONSIZE) + BUTTONSIZE)
+					{
+						if(!button[row][col].GetClicked())
+						{
+							button[row][col].SetClicked();
+							//button[row][col].SetSurrounding(GetNeighbors(row, col));
+							System.out.println(GetNeighbors(row, col));
+						}
+					}
+				}
+			}
+		}
+	}
+	public int GetNeighbors(int r, int c)
+	{
+		int count = 0;
+		for(int row = r - 1; row < ROWS && row <= r + 1; r++)
+		{
+			for(int col = c - 1; col < COLS && col <= r + 1; c++)
+			{
+				if(r >= 0 && c >= 0)
+				{
+					if(button[row][col].GetBomb())
+					{
+						count++;
+					}
+				}
+			}
+		}
+		return count;
 	}
 }
